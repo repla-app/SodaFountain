6305e67129165b204389e4b3b7ed33d84c64027b
Roben Kleene <robenk@gmail.com>
2017-05-25 20:28:27 -0400

    First commit

---
+++ PotionTaster/PotionTaster.h
@@ -0,0 +1,19 @@
+//
+//  PotionTaster.h
+//  PotionTaster
+//
+//  Created by Roben Kleene on 5/25/17.
+//  Copyright © 2017 Roben Kleene. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PotionTaster.
+FOUNDATION_EXPORT double PotionTasterVersionNumber;
+
+//! Project version string for PotionTaster.
+FOUNDATION_EXPORT const unsigned char PotionTasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PotionTaster/PublicHeader.h>
+
+
