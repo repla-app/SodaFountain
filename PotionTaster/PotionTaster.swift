d33853c55b10c5c94be3532571fbe871853b851a
Roben Kleene <robenk@gmail.com>
2017-05-25 21:52:26 -0400

    Setup initial plugins

---
+++ PotionTaster/PotionTaster.swift
@@ -0,0 +1,14 @@
+//
+//  PotionTaster.swift
+//  PotionTaster
+//
+//  Created by Roben Kleene on 5/25/17.
+//  Copyright © 2017 Roben Kleene. All rights reserved.
+//
+
+import Foundation
+
+class PotionTaster {
+    // pluginsDirectoryPath
+    // pluginsDirectoryURL
+}
