50398b58ade0cda738bcfd3d983a698443584ea5
Roben Kleene <contact@robenkleene.com>
2019-01-30 14:58:46 -0500

    Update boilerplate

--- SodaTaster/TestAssets.swift
+++ SodaTaster/TestAssets.swift
@@ -12,29 +12,42 @@ public class TestAssets {
 
     public struct File {
         public let url: URL
-        public let path: String
+        public var path: String {
+            return url.path
+        }
     }
     
     // MARK: Public
     
     // Assets
     public class var testHTMLJQUERY: File {
-        return File(url: URL(string: "")!, path: "")
+        return File(url: url(forResource: testHTMLJQUERYFilename,
+                             withExtension: testHTMLFileExtension,
+                             subdirectory: testHTMLPathComponent))
     }
 
+    public class var testHTML: File {
+        return File(url: url(forResource: testHTMLFilename,
+                             withExtension: testHTMLFileExtension,
+                             subdirectory: testHTMLPathComponent))
+    }
+
+    
     // MARK: Private
     
     // Assets
     private static let testHTMLJQUERYFilename = "indexjquery"
+    private static let testHTMLFilename = "index"
     private static let testJavaScriptFileExtension = "js"
+    private static let testHTMLFileExtension = "html"
     private static let testAssetsPathComponent = "TestAssets"
     private static let testJavaScriptPathComponent = testAssetsPathComponent.appending("js")
-    
-    // MARK: Private
+    private static let testHTMLPathComponent = testAssetsPathComponent.appending("html")
 
-    func url(forResource name: String,
-             withExtension fileExtension: String,
-             subdirectory: String) -> URL {
+    // Helper
+    private static func url(forResource name: String,
+                            withExtension fileExtension: String,
+                            subdirectory: String) -> URL {
 
         guard let fileURL = Bundle(for: TestAssets.self).url(forResource: name, withExtension: fileExtension, subdirectory: subdirectory) else {
             assert(false)
